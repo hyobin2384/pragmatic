83e18c1d88facccd7cc83558bd2a46d1dcfb5ec4
wjdgyqls <wjdgyqls4858@naver.com>
2022-02-09 15:02:59 +0900

    gogo

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -14,16 +14,14 @@ from pathlib import Path
 
 import environ
 import os
-
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(
-    # set casting, default value
-    env_file = os.path.join(BASE_DIR, '.env'),
-    DEBUG=(bool, False)
-)
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -33,6 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -84,10 +83,10 @@ WSGI_APPLICATION = 'pragmatic.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
 
 
