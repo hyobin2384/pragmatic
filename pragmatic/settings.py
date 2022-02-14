631fcba0cf678a20a246c4f5ede61f0d0e95ce98
wjdgyqls <wjdgyqls4858@naver.com>
2022-02-14 14:05:28 +0900

    django 22 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 import environ
 import os
+
+from django.urls import reverse_lazy
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
@@ -131,6 +134,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
